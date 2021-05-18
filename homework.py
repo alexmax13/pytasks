7d0c7cb751b03d871aa13c002dc4d2f1874ba6cd
alexmax13 <oleksandrmax13@gmail.com>
2021-05-19 00:21:20 +0300

    homework2

--- homework.py
+++ homework.py
@@ -1,27 +1,27 @@
-# Task 1
+#  Task 1
 name = "Sasha"
-day = " Tuesday"
+day = " Monday"
 message = f'Good day {name}!\
 {day} is a perfect day to learn some python.'
 print(message, end='\n\n\n')
 
-#Task 2
+#  Task 2
 first_name = 'Sasha'
 last_name = 'Maksiutynskyi'
 msg = f'{first_name} {last_name} "is a coder"'
 print(msg)
 
 
-# Task 3
+#  Task 3
 a = 5
 b = 2
 print(
-    a + b,  # Addition
-    a - b,  # Subtraction
-    a * b,  # Multiplication
-    a / b,  # Division
-    a // b, # Floor division
-    a % b,  # Modulus
-    a ** b, # Exponent
+    a + b,   # Addition
+    a - b,   # Subtraction
+    a * b,   # Multiplication
+    a / b,   # Division
+    a // b,  # Floor division
+    a % b,   # Modulus
+    a ** b,  # Exponent
     sep='\n'
      )
