cbcefac70e5a25673444c75b463bc61964cdcc20
alexmax13 <oleksandrmax13@gmail.com>
2021-05-20 12:39:58 +0300

    edit task 2

--- homework.py
+++ homework.py
@@ -1,18 +1,23 @@
 #  Task 1
+
 name = "Sasha"
 day = " Monday"
-message = f'Good day {name}!\
+message = f'Good day, {name}!\
 {day} is a perfect day to learn some python.'
 print(message, end='\n\n\n')
 
 #  Task 2
+
 first_name = 'Sasha'
-last_name = 'Maksiutynskyi'
-msg = f'{first_name} {last_name} "is a coder"'
-print(msg)
+last_name = ' Maksiutynskyi'
+msg_1 = f'{first_name} {last_name} is a coder!'
+msg_2 = first_name + last_name + ' is a coder!'
+print(msg_1, end='\n\n')
+print(msg_2, end='\n\n')
 
 
 #  Task 3
+
 a = 5
 b = 2
 print(
