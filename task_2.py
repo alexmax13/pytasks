267176be7b145d97936d2fb47c84ee87c35c9a19
alexmax13 <oleksandrmax13@gmail.com>
2021-05-23 21:12:26 +0300

    fix comment

--- task_2.py
+++ task_2.py
@@ -1,6 +1,6 @@
-# Task 1
+# Task 2
 
 name = input("Please enter your name:")
-age = int(input("Please enter your age:"))
+age = input("Please enter your age:")
 print('Hello', name, 'on your next birthday \
 you’ll be', age + 1, 'years')
