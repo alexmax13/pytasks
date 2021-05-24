58112fcb0a3f142679935977912339750feb8f76
alexmax13 <oleksandrmax13@gmail.com>
2021-05-24 12:45:00 +0300

    fix input

--- task_2.py
+++ task_2.py
@@ -1,6 +1,12 @@
 # Task 2
 
 name = input("Please enter your name:")
-age = input("Please enter your age:")
-print('Hello', name, 'on your next birthday \
+while True:
+    try:
+        age = int(input("Please enter your age:"))
+    except ValueError:
+        print("This is not an integer, try again.")
+    else:
+        print('Hello', name, 'on your next birthday \
 you’ll be', age + 1, 'years')
+        break
