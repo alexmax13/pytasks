b99c1d77842bedc58ed624c2d07f2da7d22cfc33
alexmax13 <oleksandrmax13@gmail.com>
2021-05-24 13:10:34 +0300

    fix input integer error

--- task_1.py
+++ task_1.py
@@ -1,16 +1,16 @@
 # Task 1
 
 import random
-
 secret_number = random.randint(1, 10)
-# We have only three try to guess.
-guess_count = 0
-guess_limit = 3
-while guess_count < guess_limit:
-    guess = input('Guess the number:')
-    guess_count += 1
-    if guess == secret_number:
-        print('You won, it was number', secret_number)
+while True:
+    try:
+        guess = int(input('Guess the number:'))
+        if guess == secret_number:
+            print('You won, it was number', secret_number)
+            break
+    except ValueError:
+        print("This is not an integer, try again")
+    else:
+        print('You failed, it was number', secret_number)
         break
-else:
-    print('You failed, it was number', secret_number)
+
