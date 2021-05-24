3a26fe85ed028b68046c701b5bc19b2ace854020
alexmax13 <oleksandrmax13@gmail.com>
2021-05-24 15:43:21 +0300

    fix input

--- task_3.py
+++ task_3.py
@@ -2,8 +2,14 @@
 import random
 count = 0
 limit = 5
-word_1 = input('Insert the word:')
-while count < limit:
-    var_2 = random.sample(word_1, len(word_1))
-    print("".join(var_2))
-    count += 1
+while True:
+    word_1 = input('Insert the word:')
+    word_check = word_1.isalpha()
+    if word_check is False:
+        continue
+    else:
+        while count < limit:
+            var_2 = random.sample(word_1, len(word_1))
+            print("".join(var_2))
+            count += 1
+        break
