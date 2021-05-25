878ac77ab1b6cae1f79df78b087303d32ed71b84
alexmax13 <oleksandrmax13@gmail.com>
2021-05-25 20:50:54 +0300

    add folders

---
+++ lesson4/task1.py
@@ -0,0 +1,15 @@
+# Task 1
+
+import random
+secret_number = random.randint(1, 10)
+while True:
+    try:
+        guess = int(input('Guess the number:'))
+        if guess == secret_number:
+            print('You won, it was number', secret_number)
+            break
+    except ValueError:
+        print("This is not an integer, try again")
+    else:
+        print('You failed, it was number', secret_number)
+        break
