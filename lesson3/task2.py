9c4e01060a2fcca7c6710b188113ed7dc3524903
alexmax13 <oleksandrmax13@gmail.com>
2021-05-27 19:55:46 +0300

    fix len of number

--- lesson3/task2.py
+++ lesson3/task2.py
@@ -1,7 +1,7 @@
 #  Task 2
 phone_number = "1234567890"
 check_var = phone_number.isdigit()
-if check_var is not True or len(phone_number) < 10:
-    print('It doesn\'t look like a phone number')
+if not check_var or len(phone_number) != 10:
+    print("It doesn't look like a phone number")
 else:
-    print("It\'s a phone number!")
+    print("It's a phone number!")
