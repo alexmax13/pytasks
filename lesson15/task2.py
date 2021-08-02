27bc0d95f0306a91712656431ca2eafdd183cd72
alexmax13 <oleksandrmax13@gmail.com>
2021-08-02 14:29:21 +0300

    fix decorators

--- lesson15/task2.py
+++ lesson15/task2.py
@@ -1,6 +1,6 @@
 # Worker has a property 'boss', and its value must be an instance of Boss.
-#
 # You can reassign this value, but you should check whether the new value is Boss.
+#
 # Each Boss has a list of his own workers.
 # You should implement a method that allows you to add workers to a Boss.
 # You're not allowed to add instances of Boss class
@@ -8,28 +8,44 @@
 
 class Boss:
     def __init__(self, id_: int, name: str, company: str):
-        self.id = id_
+        self.__id = id_
         self.name = name
         self.company = company
-        self.workers = []
+        self.__workers = []
+
+    def add_new_worker(self, new_worker):
+        if type(new_worker) == Worker and new_worker not in self.__workers:
+            self.__workers.append(new_worker)
 
     @property
     def list_workers(self):
-        print('Show list of workers')
-        return self.workers
+        return [worker.name for worker in self.__workers]
 
-    @list_workers.setter
-    def list_workers(self, worker):
-        print(f'add workers: {worker.name}')
-        self.workers.append(worker.name)
+    def __str__(self):
+        return self.name
 
 
 class Worker:
-    def __init__(self, id_: int, name: str, company: str, boss: Boss):
+    def __init__(self, id_: int, name: str, company: str, init_boss: Boss):
         self.id = id_
         self.name = name
         self.company = company
-        self.boss = boss
+        self.boss = init_boss    # same as self.boss(boss) (setter)
+
+    @property
+    def boss(self):
+        return self.__boss
+
+    @boss.setter
+    def boss(self, new_value):
+        if type(new_value) == Boss:
+            self.__boss = new_value
+            self.__boss.add_new_worker(self)
+        else:
+            print("Not Boss")
+
+    def __str__(self):
+        return self.name
 
 
 Eugene = Boss(69, 'Eugene', 'GameDev')
@@ -41,10 +57,8 @@ Naz = Worker(1, 'Naz', 'BuildFactory', Karina)
 Bob = Worker(1, 'Bob', 'Etsy', Karina)
 
 
-Eugene.list_workers = San
-Eugene.list_workers = Val
-Eugene.list_workers = Bob
+San.boss = Eugene
+Val.boss = Eugene
 print(Eugene.list_workers)
 
-Karina.list_workers = Naz
-print(Karina.list_workers)
+print(San.boss)
