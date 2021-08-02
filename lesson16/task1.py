0cfc5f357a6d20718a2ae0d2d5bbe182995a591a
alexmax13 <oleksandrmax13@gmail.com>
2021-08-02 14:36:52 +0300

    add for

--- lesson16/task1.py
+++ lesson16/task1.py
@@ -1,3 +1,9 @@
+# Create your own implementation of a built-in function enumerate,
+# named `with_index`, which takes two parameters:
+# `iterable` and `start`, default is 0.
+#
+# Tips: see the documentation for the enumerate function
+
 def with_index(iterable, start=0):
     n = start
     for element in iterable:
@@ -6,4 +12,8 @@ def with_index(iterable, start=0):
 
 
 week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-print(list(with_index(week, start=1)))
+
+# print(list(with_index(week, start=1)))            # or
+
+for i in with_index(week, start=1):
+    print(i)
