16fa5ac616cb0bc2696d349affa1f952bd0db8dd
alexmax13 <oleksandrmax13@gmail.com>
2022-04-04 22:12:50 +0300

    asdasd

--- lesson35/multiproc.py
+++ lesson35/multiproc.py
@@ -5,30 +5,29 @@ from multiprocessing import Pool
 import os
 import time
 
-# session: requests.Session = None
-# fold = None
-# timeout = None
-cwd = os.getcwd()
+mp_session: requests.Session = None
+mp_folder = None
+mp_timeout = .0
 
 
 class MultiprocDownloader:
 
-    def __init__(self, fold=None, timeout=None, session: requests.Session=None):
-        self.fold = fold
-        self.timeout = timeout
-        self.session = session
+    @staticmethod
+    def init_process(folder, timeout_):
+        global mp_session
+        global mp_folder
+        global mp_timeout
+        mp_folder = folder
+        mp_timeout = timeout_
+        if not mp_session:
+            mp_session = requests.Session()
 
-    def init_process(self, folder, timeout_):
-        if not self.session:
-            self.session = requests.Session()
-        self.fold = folder
-        self.timeout = timeout_
-
-    def get_data(self, url_, params):
+    @staticmethod
+    def get_data(url_, params):
         start_loop = datetime.datetime.now()
         while True:
-            response = self.session.get(url_, params=params)
-            if (datetime.datetime.now() - start_loop).total_seconds() > timeout:
+            response = mp_session.get(url_, params=params)
+            if (datetime.datetime.now() - start_loop).total_seconds() > mp_timeout:
                 print(f"Timeout connection: {params['subreddit']}")
                 return {}
             print(f"{multiprocessing.current_process().name}: {params['subreddit']}-{response.status_code}")
@@ -36,14 +35,15 @@ class MultiprocDownloader:
                 return response.json()
             time.sleep(2)
 
-    def worker(self, subreddit):
+    @staticmethod
+    def worker(subreddit):
         base_url = 'https://api.pushshift.io/reddit/comment/search/'
         params = {'subreddit': subreddit, "sort": "desc", "sort_type": "created_utc", "size": 5}
-        data = get_data(base_url, params)
+        data = MultiprocDownloader.get_data(base_url, params)
         if 'data' not in data:
             print('Empty data')
             return
-        with open(f"{self.fold}/{subreddit}.txt", "w", encoding='utf-8') as file:
+        with open(f"{mp_folder}/{subreddit}.txt", "w", encoding='utf-8') as file:
             for i in data['data']:
                 comment = 'Author: ' + i['author'] + '\n' \
                           + 'Comment: ' + i['body'] + '\n' \
@@ -51,22 +51,28 @@ class MultiprocDownloader:
                     '%Y-%m-%d %H:%M:%S') + '\n'
                 file.write(comment + '\n')
 
+    def launch(self):
+        start = datetime.datetime.now()
+        cwd = os.getcwd()
+        with open(f'{cwd}/subreddits.txt', 'r') as f:
+            subreddits_list = f.read().splitlines()
+
+        timeout = len(subreddits_list) * 3
+
+        date = datetime.datetime.now().strftime('%d-%m-%y %H-%M-%S')
+        folder_ = os.path.join(cwd, f'{date}')
+        if not os.path.exists(folder_):
+            os.mkdir(folder_)
+
+        with Pool(initializer=MultiprocDownloader.init_process, initargs=(folder_, timeout)) as pool:
+            pool.map(MultiprocDownloader.worker, subreddits_list)
+
+        print(datetime.datetime.now() - start)
+
 
 if __name__ == '__main__':
 
-    start = datetime.datetime.now()
+    l = MultiprocDownloader()
+    l.launch()
 
-    with open(f'{cwd}/subreddits.txt', 'r') as f:
-        subreddits_list = f.read().splitlines()
 
-    timeout = len(subreddits_list) * 3
-
-    date = datetime.datetime.now().strftime('%d-%m-%y %H-%M-%S')
-    folder_ = os.path.join(cwd, f'{date}')
-    if not os.path.exists(folder_):
-        os.mkdir(folder_)
-
-    with Pool(initializer=init_process, initargs=(folder_, timeout)) as pool:
-        pool.map(self.worker, subreddits_list)
-
-    print(datetime.datetime.now() - start)
