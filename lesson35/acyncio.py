2c3deb6f55f7e00df4f07277ef6f5320d3892d3b
alexmax13 <oleksandrmax13@gmail.com>
2022-04-08 20:24:36 +0300

    add interface to contest

--- lesson35/acyncio.py
+++ lesson35/acyncio.py
@@ -3,23 +3,27 @@ import aiohttp
 import aiofiles
 import os
 import datetime
+from interface import DownloadCommentsInterFace
 
 
-class AsyncDownloader:
+class AsyncDownloader(DownloadCommentsInterFace):
 
-    def __init__(self, cwd=os.getcwd(), folder=None, timeout=.0):
-        self.cwd = cwd
-        self.folder_ = folder
-        self.timeout = timeout
+    def __init__(self):
+        self.timeout = .0
+        self.folder = ''
 
     async def get_data(self, url_, params, session):
         start_loop = datetime.datetime.now()
         while True:
-            response = await session.get(url_, params=params, ssl=False)
+            try:
+                response = await session.get(url_, params=params, ssl=False)
+            except:
+                print(f"async GET error occurred: {params['subreddit']}")
             if (datetime.datetime.now() - start_loop).total_seconds() > self.timeout:
                 print(f"Timeout connection: {params['subreddit']}")
                 return {}
             if response.status == 200:
+                print(f"{params['subreddit']}: {response.status}")
                 return await response.json()
             await asyncio.sleep(2)
 
@@ -28,7 +32,7 @@ class AsyncDownloader:
         params = {'subreddit': subreddit, "sort": "desc", "sort_type": "created_utc", "size": 5}
         data = await self.get_data(base_url, params, session)
 
-        async with aiofiles.open(f"{self.folder_}/{subreddit}.txt", "w", encoding='utf-8') as file:
+        async with aiofiles.open(f"{self.folder}/{subreddit}.txt", "w", encoding='utf-8') as file:
             for i in data['data']:
                 comment = 'Author: ' + i['author'] + '\n' \
                           + 'Comment: ' + i['body'] + '\n' \
@@ -36,34 +40,35 @@ class AsyncDownloader:
                     '%Y-%m-%d %H:%M:%S') + '\n'
                 await file.write(comment + '\n')
 
-    def launch(self):
-        start = datetime.datetime.now()
+    def launch(self, subreddits, folder):
+        self.timeout = len(subreddits) * 3
+        self.folder = folder
+        asyncio.run(self.main(subreddits))
 
-        date = datetime.datetime.now().strftime('%d-%m-%y %H-%M-%S')
-        self.folder_ = os.path.join(self.cwd, f'{date}')
-        if not os.path.exists(self.folder_):
-            os.mkdir(self.folder_)
-
-        # asyncio.get_event_loop().run_until_complete(self.main())
-        asyncio.run(self.main())
-
-        print(datetime.datetime.now() - start)
-
-    async def main(self):
-        with open(f'{self.cwd}/subreddits.txt', 'r') as f:
-            subreddits_list = f.read().splitlines()
-
-        self.timeout = len(subreddits_list) * 3
-
+    async def main(self, subreddits):
         async with aiohttp.ClientSession() as session:
             tasks = []
-            for subreddit in subreddits_list:
+            for subreddit in subreddits:
                 tasks.append(self.worker(subreddit, session))
             await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
 
+    cwd = os.getcwd()
+
+    date = datetime.datetime.now().strftime('%d-%m-%y (%H-%M-%S)')
+    folder_ = os.path.join(cwd, f'{date}')
+    if not os.path.exists(folder_):
+        os.mkdir(folder_)
+
+    with open(f'{cwd}/subreddits.txt', 'r') as f:
+        subreddits_list = f.read().splitlines()
+
+    start = datetime.datetime.now()
+
     downloader = AsyncDownloader()
-    downloader.launch()
+    downloader.launch(subreddits_list, folder_)
+
+    print(datetime.datetime.now() - start)
 
